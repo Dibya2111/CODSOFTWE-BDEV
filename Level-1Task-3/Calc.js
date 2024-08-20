02cf19c5e178e64805d61b01e48dfe85d8f211c3
Dibya2111 <dibyajyotidash88@gmail.com>
2024-08-20 20:40:13 +0530

    Calculator Task Completed

---
+++ Level-1Task-3/Calc.js
@@ -0,0 +1,87 @@
+let display = document.getElementById('display');
+let currentInput = '';
+let lastInput = '';
+
+function appendNumber(number) {
+    if (lastInput === '=') {
+        clearDisplay();
+        lastInput = '';
+    }
+    if (number === '.' && currentInput.indexOf('.') !== -1) {
+        return;
+    }
+    currentInput = currentInput + number;
+    display.innerText = currentInput;
+}
+
+function appendOperator(operator) {
+    if (lastInput === '=') {
+        lastInput = '';
+    }
+    if (currentInput === '') return;
+    if (checkOperator(currentInput[currentInput.length - 1])) {
+        currentInput = currentInput.substring(0, currentInput.length - 1);
+    }
+    currentInput = currentInput + operator;
+    display.innerText = currentInput;
+}
+
+function checkOperator(character) {
+    return character === '+' || character === '-' || character === '*' || character === '/';
+}
+
+function clearDisplay() {
+    currentInput = '';
+    display.innerText = '00';
+}
+
+function deleteLast() {
+    currentInput = currentInput.substring(0, currentInput.length - 1);
+    display.innerText = currentInput;
+}
+
+function addDecimal() {
+    if (lastInput === '=') {
+        clearDisplay();
+        lastInput = '';
+    }
+    if (currentInput.indexOf('.') === -1) {
+        currentInput = currentInput + '.';
+        display.innerText = currentInput;
+    }
+}
+
+function calculate() {
+    try {
+        let result = 0;
+        let currentNumber = '';
+        let operation = '+';
+
+        for (let i = 0; i < currentInput.length; i++) {
+            let char = currentInput[i];
+            if ((char >= '0' && char <= '9') || char === '.') {
+                currentNumber += char;
+            } else {
+                result = doMath(result, parseFloat(currentNumber), operation);
+                currentNumber = '';
+                operation = char;
+            }
+        }
+
+        result = doMath(result, parseFloat(currentNumber), operation);
+        currentInput = result.toString();
+        display.innerText = currentInput;
+        lastInput = '=';
+    } catch (error) {
+        display.innerText = 'Error';
+        currentInput = '';
+    }
+}
+
+function doMath(left, right, op) {
+    if (op === '+') return left + right;
+    if (op === '-') return left - right;
+    if (op === '*') return left * right;
+    if (op === '/') return left / right;
+    return right;
+}
